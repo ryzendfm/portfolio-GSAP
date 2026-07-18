34d333c9d6d2d1842769aa12460b9d0c9d09b7cf
Your Name <you@example.com>
2026-07-18 09:19:30 +0530

    commit part 3: images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,63 @@
+import profile from './profile.png';
+import profile2 from './profile.png';
+import esmail from './esmail.webp';
+import mohammad from './mohammad.webp';
+import workBg1 from './1.webp';
+import workBg2 from './2.webp';
+import workBg3 from './3.webp';
+import workBg4 from './4.webp';
+import workBg5 from './5.webp';
+import blenderCar from './blender-car.jpg';
+import skyStudy1 from './sky-study-1.jpeg';
+import skyStudy2 from './sky-study-2.jpeg';
+import skyStudy3 from './sky-study-3.jpeg';
+import closeupEyeShot from './closeup eye shot.jpeg';
+import guyDrawing from './guy drawing.jpeg';
+import yuiYuigahamaDrawing from './yui yuigahama drawing.jpeg';
+import girlSketch from './girl sketch.png';
+import guySketchScreenshot from './guy sketch screen shot.jpeg';
+import sketchScreenshot from './sketch screenshot.jpeg';
+import skyPhoto1 from './UniversalUpscaler_7bab7b7b-6a37-4de2-865a-cc88cb48c851.jpg.jpeg';
+import skyPhoto2 from './1000536345.jpeg';
+import skyPhoto3 from './overcast sky.jpeg';
+import astroPhoto1 from './1000263540.jpg.jpeg';
+import astroPhoto2 from './deep space.jpeg';
+import travelPhoto1 from './vagamon.jpeg';
+import travelPhoto2 from './safari.jpeg';
+import travelPhoto3 from './vybin beach.jpeg';
+import gfxPhoto1 from './gfx.jpg';
+import gfxPhoto2 from './gfx 1.jpg';
+import gfxPhoto3 from './poster.jpg';
+
+export {
+  profile,
+  profile2,
+  esmail,
+  mohammad,
+  workBg1,
+  workBg2,
+  workBg3,
+  workBg4,
+  workBg5,
+  blenderCar,
+  skyStudy1,
+  skyStudy2,
+  skyStudy3,
+  closeupEyeShot,
+  guyDrawing,
+  yuiYuigahamaDrawing,
+  girlSketch,
+  guySketchScreenshot,
+  sketchScreenshot,
+  skyPhoto1,
+  skyPhoto2,
+  skyPhoto3,
+  astroPhoto1,
+  astroPhoto2,
+  travelPhoto1,
+  travelPhoto2,
+  travelPhoto3,
+  gfxPhoto1,
+  gfxPhoto2,
+  gfxPhoto3,
+};
